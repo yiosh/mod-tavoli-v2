b302bc710a0df4dd068bea3b52e855c84e92ce59
Yiosh <iinoted@gmail.com>
2019-03-08 14:02:54 +0100

    Clean unused dependencies

--- src/main.js
+++ src/main.js
@@ -2,15 +2,8 @@ import Vue from "vue";
 import "./plugins/vuetify";
 import App from "./App.vue";
 import VueKonva from "vue-konva";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faCircle, faSquare } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import store from "./store/store";
 import "nprogress/nprogress.css";
-
-library.add(faCircle, faSquare);
-
-Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.use(VueKonva);
 
